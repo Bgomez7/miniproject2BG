b7777db7d25c6e008a6922fd1a1505c2033d5089
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2023-09-24 00:09:23 -0500

    Added graph_mathgpa to main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def files_found():
 def get_states():
     states = []
     # get 5 states
-    for i in range(1,6):
+    for i in range(1,3):
         # verify the states
         while True:
             print(f"Enter state(format:\"Kansas\"): ")
@@ -52,13 +52,32 @@ def get_states():
     return states
 
 
-def get_states_mathgpa(states):
-    states_data = []
-    for i, state in enumerate(states):
-        states_data.append(df_math_gpa.loc[df_math_gpa["State.Name"] == state])
-        states_data[i] = states_data[i].reset_index(drop=True)
+def get_states_mathgpa(state):
+    state_gpa = (df_math_gpa.loc[df_math_gpa["State.Name"] == state])
+    state_gpa = state_gpa.reset_index(drop=True)
 
-    return states_data
+    return state_gpa
+
+
+def graph_mathgpa(state):
+    y_axis = [gpa for gpa in state["Academic Subjects.Mathematics.Average GPA"]]
+    x_axis = [year for year in state["Year"]]
+    # plot the graph
+    plt.plot(x_axis, y_axis)
+
+    # get min,max of gpa(y-axis) for nicer graph
+    gpa_range = y_axis
+    gpa_range.sort()
+    plt.axis([x_axis[0], x_axis[-1], gpa_range[0] - .05, gpa_range[-1] + .05])
+    # show every year
+    plt.xticks(x_axis)
+
+    # labels for the graph
+    plt.title(f"Average GPA for Math in a Year for {state}")
+    plt.xlabel("Year")
+    plt.ylabel("GPA")
+
+    plt.show()
 
 
 # CSV is found and saved to a panda dataframe
@@ -67,4 +86,4 @@ df = pd.DataFrame(pd.read_csv("school_scores.csv"))
 df_math_gpa = df[["Year", "State.Name" , "Academic Subjects.Mathematics.Average GPA"]]
 
 for state in get_states():
-    get_states_mathgpa(state)
+    graph_mathgpa(get_states_mathgpa(state))
